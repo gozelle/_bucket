4c32cc1c805669423621652722a95487039528cc
koyeo <koyeo@qq.com>
2021-07-06 17:15:42 +0800

    update

--- bucket.go
+++ bucket.go
@@ -1,4 +1,4 @@
-package bucket
+package _robot
 
 import (
 	"sync"
@@ -6,20 +6,18 @@ import (
 )
 
 func NewBucket(duration time.Duration) *Bucket {
-
 	b := new(Bucket)
 	b.mu = new(sync.Mutex)
 	b.duration = duration
 	b.first = true
 	b.change = false
 	b.setNext()
-
 	return b
 }
 
 type Bucket struct {
 	duration time.Duration
-	messages []Message
+	messages []interface{}
 	mu       *sync.Mutex
 	next     int64
 	change   bool
@@ -34,43 +32,45 @@ func (p *Bucket) setNext() {
 	p.next = p.now() + int64(p.duration)
 }
 
-func (p *Bucket) call(callback func(messages []Message)) {
-
+func (p *Bucket) call(callback func(messages []interface{})) {
 	p.mu.Lock()
-	messages := make([]Message, len(p.messages))
+	defer p.mu.Unlock()
+	
+	messages := make([]interface{}, len(p.messages))
 	copy(messages, p.messages)
-	p.mu.Unlock()
-
 	callback(messages)
-	p.messages = make([]Message, 0)
+	p.messages = make([]interface{}, 0)
 	p.setNext()
 }
 
-func (p *Bucket) Push(message Message) {
+func (p *Bucket) Push(message interface{}) {
 	p.mu.Lock()
+	defer p.mu.Unlock()
+	
 	if len(p.messages) == 0 {
 		p.change = true
 	} else {
 		p.change = false
 	}
 	p.messages = append(p.messages, message)
-	p.mu.Unlock()
+	
 }
 
-func (p *Bucket) PushWait(message Message, wait time.Duration) {
-	p.mu.Lock()
-	p.next = p.now() + int64(wait)
-	if len(p.messages) == 0 {
-		p.change = true
-	} else {
-		p.change = false
-	}
-	p.messages = append(p.messages, message)
-	p.mu.Unlock()
-}
+// PushWait 推入消息同时增加下次释放间隔
+//func (p *Bucket) PushWait(message interface{}, wait time.Duration) {
+//	p.mu.Lock()
+//	p.next = p.now() + int64(wait)
+//	if len(p.messages) == 0 {
+//		p.change = true
+//	} else {
+//		p.change = false
+//	}
+//	p.messages = append(p.messages, message)
+//	p.mu.Unlock()
+//}
 
-// 将消息全部弹出
-func (p *Bucket) Pop(callback func(messages []Message)) {
+// PopLazily 所有的消息等待时间间隔结束后推出
+func (p *Bucket) PopLazily(callback func(messages []interface{})) {
 	for {
 		if p.now() >= p.next {
 			p.call(callback)
@@ -78,7 +78,8 @@ func (p *Bucket) Pop(callback func(messages []Message)) {
 	}
 }
 
-func (p *Bucket) First(callback func(messages []Message)) {
+// PopTimely 一个时间间隔里的进入的第一条消息瞬时推出， 其余消息等待时间间隔结束后推出
+func (p *Bucket) PopTimely(callback func(messages []interface{})) {
 	for {
 		if p.change && p.first {
 			p.first = false
--- bucket_test.go
+++ bucket_test.go
@@ -1,27 +1,52 @@
-package bucket
+package _robot
 
 import (
-	"fmt"
 	"testing"
 	"time"
 )
 
-type M string
-
-func (m M) String() string {
-	return string(m)
-}
-
 func TestNewBucket(t *testing.T) {
-	bucket := NewBucket(2 * time.Second)
+	bucket := NewBucket(5 * time.Second)
 	go func() {
 		for {
-			bucket.Push(M(time.Now().String()))
-			time.Sleep(600 * time.Millisecond)
+			bucket.Push(time.Now().String())
+			time.Sleep(500 * time.Millisecond)
 		}
 	}()
-
-	bucket.First(func(messages []Message) {
-		fmt.Println(len(messages), messages)
+	
+	bucket.PopTimely(func(messages []interface{}) {
+		t.Log(len(messages), messages)
 	})
 }
+
+func TestNewBucket2(t *testing.T) {
+	bucket := NewBucket(5 * time.Second)
+	go func() {
+		for {
+			bucket.Push(time.Now().String())
+			time.Sleep(500 * time.Millisecond)
+		}
+	}()
+	
+	bucket.PopLazily(func(messages []interface{}) {
+		t.Log(len(messages), messages)
+	})
+}
+
+//func TestNewBucket3(t *testing.T) {
+//	bucket := NewBucket(1 * time.Second)
+//
+//	go func() {
+//		i := 0
+//		for {
+//			i++
+//			bucket.PushWait(time.Now().String(), time.Duration(i)*time.Second)
+//			//time.Sleep(500 * time.Millisecond)
+//			time.Sleep(1 * time.Second)
+//		}
+//	}()
+//
+//	bucket.PopLazily(func(messages []interface{}) {
+//		t.Log(len(messages), messages)
+//	})
+//}
